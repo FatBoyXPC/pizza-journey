62dafd542e56dc2735103ed9850f01ea291db508
James LaChance <fatboyxpc@gmail.com>
2020-09-29 02:45:41 -0400

    General cleanup
    Finish updating config, remove default posts, write the about page.

--- config.php
+++ config.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'siteName' => 'My Pizza Journey',
+    'siteDescription' => 'Blog about my journey learning how to make Neapolitan pizza.',
+    'siteAuthor' => 'James LaChance',
 
     // collections
     'collections' => [
         'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
+            'author' => 'James LaChance', // Default author, if not provided in a post
             'sort' => '-date',
             'path' => 'blog/{filename}',
         ],
--- source/about.blade.php
+++ source/about.blade.php
@@ -7,13 +7,96 @@ description: A little bit about the site
 @section('body')
     <h1>About</h1>
 
-    <img src="/assets/img/about.png"
-        alt="About image"
+    <img src="{{ $page->baseUrl }}/assets/img/damichele.png"
+        alt="L'Antica Pizzeria da Michele"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+    <p class="mb-6">
+        I recently received an
+        <a href="https://ooni.com/products/ooni-fyra" target="_blank">Ooni Fyra pizza oven</a>
+        as a gift, and I've decided to write about my journey to making a good
+        Neapolitan pizza, in part because it's easier to share this way, and it
+        will be a good way to keep track of my progress.
+    </p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+    <h2>Why Pizza Making?</h2>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p class="mb-6">
+        If you had asked me a few years ago what a blog of this title would be
+        about, it likely would have been about finally finding a good pizza.
+        I'm from the midwest, which isn't really known for pizza, except for the
+        weird reason that Chicago's pizza casserole (inside out deep dish) has
+        quite the notoriety.
+    </p>
+
+    <p class="mb-6">
+        I was constantly unhappy with all of the pizza I had. I ended up in New
+        York City back in 2014 and even the 3 random pizza places I tried then
+        didn't impress me. I intentionally avoided looking anything up online
+        so that I could avoid overhyped and well marketed places, and trusted
+        some locals to guide me. Turns out maybe I should have done both, as I
+        still hadn't found anything I like.
+    </p>
+
+    <p class="mb-6">
+        In 2015 I ended up in Atlanta, and in a random set of events, at
+        <a href="http://www.varasanos.com/" target="_blank">Varasano's Pizzeria</a>. Going in I wasn't
+        expecting to be that impressed, as I had always been a bit disappointed
+        with pizza. I saw on the menu that this was a thin crust pizza, and I
+        feared a crunchy, crumbly, cracker like crust. I couldn't have been
+        more wrong. This pizza was soft, not crunchy. It was light. The crust
+        was springy and chewy (in a good way). It was everything opposite of
+        what I previously thought of as a "thin crust pizza", yet, the crust
+        was still somewhat thin. It's hard to explain, but to say the least, I
+        absolutely fell in love with this pizza. The owner, Jeff, is also very
+        helpful. He has quite a few videos on his
+        <a href="https://www.youtube.com/user/varasano/videos" target="_blank">YouTube Channel</a>
+        showing you how to stretch dough, bake the pizzas, etc. He even has
+        <a href="http://www.varasanos.com/PizzaRecipe.htm" target="_blank">his recipe</a>
+        and experimentation results online.
+    </p>
+
+    <p class="mb-6">
+        This is when I learned what Neapolitan pizza (or pizza napoletana, if
+        you want to sound cultured) was. I had no idea this was its own style
+        of pizza. After speaking with Jeff, though, I learned there was an
+        entire world of pizzas like this out there, I just didn't know what to
+        look for.
+    </p>
+
+    <p class="mb-6">
+        I started focusing on this type of pizza and it absolutely changed how
+        much I enjoyed pizza. I was finally happy with (most of) the pizza
+        places I found. In June 2019 I finally made my way to Napoli, or
+        Naples, Italy. I can honestly say that clearly my preferred style of
+        pizza is this, and that it's also difficult (though not impossible) to
+        find a "bad" pizza in Napoli.
+    </p>
+
+    <img src="{{ $page->baseUrl }}/assets/img/varasanos.png"
+        alt="Varasano's Pizzeria"
+        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+
+    <p class="mb-6">
+        In all honesty, Varasano's pizza is so good that I could believe you
+        teleported me a pizza right from Napoli. Don't believe me? The image at
+        the top of this page is from L'Antica Pizzeria da Michele. The image
+        next to this paragraph is from Varasano's. Eating this pizza took me
+        right back to my trip to Napoli. This is a bit unfair of a comparison
+        as Jeff normally cooks his pizza just a little bit differently, but
+        this is proof that he is more than capable of bringing a piece of
+        Napoli over to the US.
+    </p>
+
+    <p class="mb-6">
+        Varasano's has left me craving more, but I can't seem to find the same
+        quality of pizza in Tampa. The saying goes that if you want the job
+        done right to do it yourself. I've taken that attitude with BBQ, and
+        had always planned to eventually start making my own pizza, but I
+        wasn't prioritizing it. Since a good friend of mine decided to give me
+        such a gift (knowing how snobby about pizza I am), I'm going to finally
+        learn how to make such a prized pizza. My goal is to make a pizza that
+        sends me back to Napoli, just like Jeff's pizza did. I hope you enjoy
+        watching my journey!
+    </p>
 @endsection
